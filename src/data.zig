482f96e088191998e9e1c0584709e6a8b7655d6d
Danh Phan <input@danh.computer>
2023-09-24 16:17:12 -0500

    Add CITM Catalog

--- src/data.zig
+++ src/data.zig
@@ -1,30 +1,90 @@
+const std = @import("std");
+
+const HashMap = std.AutoHashMap;
+const StringHashMap = std.StringHashMap;
+
+const String = []const u8;
+
 pub const Pastries = []struct {
-    id: []const u8,
-    type: []const u8,
-    name: []const u8,
+    id: String,
+    type: String,
+    name: String,
     ppu: f64,
     batters: struct {
         batter: []struct {
-            id: []const u8,
-            type: []const u8,
+            id: String,
+            type: String,
         },
     },
     topping: []struct {
-        id: []const u8,
-        type: []const u8,
+        id: String,
+        type: String,
     },
 };
 
 pub const Canada = struct {
-    type: []const u8,
+    type: String,
     features: []struct {
-        type: []const u8,
+        type: String,
         properties: struct {
-            name: []const u8,
+            name: String,
         },
         geometry: struct {
-            type: []const u8,
+            type: String,
             coordinates: [][][2]f64,
         },
     },
+};
+
+pub const CITM = struct {
+    areaNames: HashMap(i64, String),
+    audienceSubCategoryNames: HashMap(i64, String),
+    blockNames: HashMap(i64, String),
+    events: HashMap(i64, Events),
+    performances: []Performances,
+    seatCategoryNames: HashMap(u64, String),
+    subTopicNames: HashMap(u64, String),
+    subjectNames: HashMap(u64, String),
+    topicNames: HashMap(u64, String),
+    topicSubTopics: HashMap(u64, []u64),
+    venueNames: StringHashMap(String),
+
+    const Events = struct {
+        description: String,
+        id: isize,
+        logo: String,
+        name: String,
+        subTopicIds: []isize,
+        subjectCode: ?String,
+        subtitle: ?String,
+        topicIds: []isize,
+    };
+
+    const Performances = struct {
+        eventId: isize,
+        id: isize,
+        logo: ?String,
+        name: ?String,
+        prices: []Prices,
+        setCategories: []SeatCategories,
+        seatMapImage: ?String,
+        start: i64,
+        venueCode: String,
+
+        const Prices = struct {
+            amount: isize,
+            audienceSubCategoryId: i64,
+            setCategoryId: i64,
+        };
+
+        const SeatCategories = struct {
+            areas: []Areas,
+            setCategoryId: isize,
+        };
+
+        const Areas = struct {
+            areaId: isize,
+            blockIds: []isize,
+        };
+    };
 };
--- src/lib.zig
+++ src/lib.zig
@@ -8,25 +8,28 @@ const heap = std.heap;
 const mem = std.mem;
 
 test "deserialize" {
-    const n_deserializations: comptime_int = 100_000;
+    const n_deserializations: comptime_int = 2;
 
     try bench.run(struct {
         pub const allocator = heap.c_allocator;
         pub const types = [_]type{
             data.Pastries,
             data.Canada,
+            data.CITM,
         };
         pub const args = [_][]const u8{
             @embedFile("data/pastries.json"),
             @embedFile("data/canada_geometry.json"),
+            @embedFile("data/citm_catalog.json"),
         };
         pub const names = [_][]const u8{
             "Pastries",
             "Canada Geometry",
+            "CITM Catalog",
         };
 
-        pub const min_iterations = 10;
-        pub const max_iterations = 10;
+        pub const min_iterations = 1;
+        pub const max_iterations = 1;
 
         pub fn benchStd(ally: mem.Allocator, comptime T: type, input: []const u8) !void {
             for (0..n_deserializations) |_| {
