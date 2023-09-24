fc574fe463095c9506f2c45b8787f6a733d6a59b
Danh Phan <input@danh.computer>
2023-09-24 11:26:54 -0500

    Set up dependencies

--- build.zig
+++ build.zig
@@ -1,47 +1,27 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+const package_name = "json-benchmark";
+const package_path = "src/lib.zig";
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "json-benchmark",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+    // Dependencies.
+    const dep_opts = .{ .target = target, .optimize = optimize };
+
+    const json_module = b.dependency("json", dep_opts).module("json");
+
+    // Tests.
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = package_path },
         .target = target,
         .optimize = optimize,
     });
+    tests.addModule("json", json_module);
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    const run_tests = b.addRunArtifact(tests);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_main_tests = b.addRunArtifact(main_tests);
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+    const test_step = b.step("test", "Run benchmarks");
+    test_step.dependOn(&run_tests.step);
 }
