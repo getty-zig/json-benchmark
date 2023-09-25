454691abe682a709ed2ec40ad184c6379b6f9d67
Danh Phan <input@danh.computer>
2023-09-24 22:53:41 -0500

    Skip CITM Catalog test for std.json

--- src/lib.zig
+++ src/lib.zig
@@ -8,8 +8,10 @@ const heap = std.heap;
 const mem = std.mem;
 
 test "deserialize" {
-    const n_deserializations: comptime_int = 2;
+    const deserializations: comptime_int = 100_000;
+    const iterations: comptime_int = 10;
 
+    // Getty JSON
     try bench.run(struct {
         pub const allocator = heap.c_allocator;
         pub const types = [_]type{
@@ -28,11 +30,33 @@ test "deserialize" {
             "CITM Catalog",
         };
 
-        pub const min_iterations = 1;
-        pub const max_iterations = 1;
+        pub const min_iterations = iterations;
+        pub const max_iterations = iterations;
 
-        pub fn benchStd(ally: mem.Allocator, comptime T: type, input: []const u8) !void {
-            for (0..n_deserializations) |_| {
+        pub fn benchGetty(
+            ally: mem.Allocator,
+            comptime T: type,
+            input: []const u8,
+        ) !void {
+            for (0..deserializations) |_| {
+                const result = try json.fromSlice(ally, T, input);
+                defer result.deinit();
+            }
+        }
+
+        // NOTE: Not all test data can be benchmarked using std.json's due to
+        // its lack of support for various types within the standard library.
+        pub fn benchStd(
+            ally: mem.Allocator,
+            comptime T: type,
+            input: []const u8,
+        ) !void {
+            switch (T) {
+                data.CITM => return error.Skipped,
+                else => {},
+            }
+
+            for (0..deserializations) |_| {
                 const output = try std.json.parseFromSlice(
                     T,
                     ally,
@@ -40,13 +64,6 @@ test "deserialize" {
                     .{ .allocate = .alloc_always },
                 );
                 defer output.deinit();
-            }
-        }
-
-        pub fn benchGetty(ally: mem.Allocator, comptime T: type, input: []const u8) !void {
-            for (0..n_deserializations) |_| {
-                const result = try json.fromSlice(ally, T, input);
-                defer result.deinit();
             }
         }
     });
