a01e2cdaba25139c03b8523adf65b729ffb70849
Danh Phan <input@danh.computer>
2023-11-07 08:20:23 -0600

    test -> bench

--- build.zig
+++ build.zig
@@ -4,24 +4,24 @@ const package_name = "json-benchmark";
 const package_path = "src/lib.zig";
 
 pub fn build(b: *std.build.Builder) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    const opts = .{
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast }),
+    };
 
-    // Dependencies.
-    const dep_opts = .{ .target = target, .optimize = optimize };
+    // Dependencies
+    const json_module = b.dependency("json", opts).module("json");
 
-    const json_module = b.dependency("json", dep_opts).module("json");
-
-    // Tests.
-    const tests = b.addTest(.{
+    // Tests
+    const bench = b.addTest(.{
+        .name = "benchmark",
         .root_source_file = .{ .path = package_path },
-        .target = target,
-        .optimize = optimize,
+        .target = opts.target,
+        .optimize = opts.optimize,
     });
-    tests.addModule("json", json_module);
+    bench.addModule("json", json_module);
 
-    const run_tests = b.addRunArtifact(tests);
-
-    const test_step = b.step("test", "Run benchmarks");
-    test_step.dependOn(&run_tests.step);
+    const bench_step = b.step("bench", "Run benchmarks");
+    const run_bench = b.addRunArtifact(bench);
+    bench_step.dependOn(&run_bench.step);
 }
