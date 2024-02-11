7200a8ad08669a53ff6205f12f4a96dae50a8170
Danh Phan <input@danh.computer>
2024-02-10 22:18:44 -0600

    build: Update std usage

--- build.zig
+++ build.zig
@@ -3,23 +3,25 @@ const std = @import("std");
 const package_name = "json-benchmark";
 const package_path = "src/lib.zig";
 
-pub fn build(b: *std.build.Builder) void {
-    const opts = .{
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast }),
-    };
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .ReleaseFast,
+    });
 
-    // Dependencies
-    const json_module = b.dependency("json", opts).module("json");
+    // Dependencies.
+    const dep_opts = .{ .target = target, .optimize = optimize };
+
+    const json_module = b.dependency("json", dep_opts).module("json");
 
     // Tests
     const bench = b.addTest(.{
         .name = "benchmark",
         .root_source_file = .{ .path = package_path },
-        .target = opts.target,
-        .optimize = opts.optimize,
+        .target = target,
+        .optimize = optimize,
     });
-    bench.addModule("json", json_module);
+    bench.root_module.addImport("json", json_module);
 
     const bench_step = b.step("bench", "Run benchmarks");
     const run_bench = b.addRunArtifact(bench);
